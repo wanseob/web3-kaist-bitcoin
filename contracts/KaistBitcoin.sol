2f3dbca363b37f08ca90d49601dddcfc2e976ec6
Wanseob Lim <email@wanseob.com>
2023-03-29 03:15:28 +0900

    step 1

--- contracts/KaistBitcoin.sol
+++ contracts/KaistBitcoin.sol
@@ -1,10 +1,29 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.0;
 
-import "hardhat/console.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract KaistBitcoin {
-    constructor() { 
-        console.log("Hello Kaist!");
+    struct KaistBitcoinTx {
+        address from;
+        address to;
+        uint256 amount;
+        uint256 fee;
+        bytes signature;
+    }
+
+    function validateTx(KaistBitcoinTx memory _tx) public pure returns (bool) {
+        bytes32 txHash = _txHash(_tx);
+        bytes32 hashToSign = ECDSA.toEthSignedMessageHash(txHash);
+        address signer = ECDSA.recover(hashToSign, _tx.signature);
+        require(_tx.from == signer, "Invalid signature");
+        return true;
+    }
+
+    function _txHash(
+        KaistBitcoinTx memory _tx
+    ) internal pure returns (bytes32) {
+        return
+            keccak256(abi.encodePacked(_tx.from, _tx.to, _tx.amount, _tx.fee));
     }
 }
--- test/KaistBitcoin.test.ts
+++ test/KaistBitcoin.test.ts
@@ -5,15 +5,26 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { KaistBitcoin, KaistBitcoin__factory } from "../typechain";
+import { parseEther } from "ethers/lib/utils";
+import { buildTx } from "../src/utils";
 
 describe("Test Kaist Bitcoin Contract", function () {
   let kaistBitcoin: KaistBitcoin;
   let deployer: SignerWithAddress;
+  let alice: SignerWithAddress;
+  let bob: SignerWithAddress;
   this.beforeEach(async () => {
-    [deployer] = await ethers.getSigners();
+    [deployer, alice, bob] = await ethers.getSigners();
     kaistBitcoin = await new KaistBitcoin__factory(deployer).deploy();
   });
-  it("should deploy a contract", async function () {
-    expect(kaistBitcoin.address).to.be.a.string
+  it("validate tx", async function () {
+    const tx = await buildTx({
+      signer: alice,
+      to: bob.address,
+      amount: parseEther("1"),
+      fee: parseEther("0.01"),
+    });
+    const result = await kaistBitcoin.validateTx(tx);
+    expect(result).to.be.true;
   });
 });
