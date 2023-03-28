75a8355f4e8677c7b79b95e771b57ea807ed73fa
Wanseob Lim <email@wanseob.com>
2023-03-29 03:35:45 +0900

    add instruction

--- contracts/KaistBitcoin.sol
+++ contracts/KaistBitcoin.sol
@@ -4,7 +4,21 @@ pragma solidity >=0.8.0;
 import "hardhat/console.sol";
 
 contract KaistBitcoin {
-    constructor() { 
-        console.log("Hello Kaist!");
+    struct KaistBitcoinTx {
+        address from;
+        address to;
+        uint256 amount;
+        uint256 fee;
+        bytes signature;
+    }
+
+    function validateTx(KaistBitcoinTx memory _tx) public pure returns (bool) {
+        // verify signature
+    }
+
+    function _txHash(
+        KaistBitcoinTx memory _tx
+    ) internal pure returns (bytes32) {
+        // return the hash of the transaction
     }
 }
