a13a3bfd18c034d093e551ac6d8f895a8a0b9463
Wanseob Lim <email@wanseob.com>
2023-03-29 03:42:22 +0900

    step2 instruction

--- contracts/KaistBitcoin.sol
+++ contracts/KaistBitcoin.sol
@@ -12,12 +12,40 @@ contract KaistBitcoin {
         bytes signature;
     }
 
+    struct KaistBitcoinBlockHeader {
+        address miner;
+        bytes32 prevHeader;
+        uint256 nonce;
+        bytes32 txRoot;
+    }
+
+    function validateBlockHeader(
+        KaistBitcoinBlockHeader memory header
+    ) public view returns (bool) {
+        bytes32 blockHash = _blockHash(header);
+        // check prev header equals to the last submitted block;
+        return validatePoW(blockHash, header.nonce);
+    }
+
+    function validatePoW(
+        bytes32 blockHash,
+        uint256 nonce
+    ) public view returns (bool) {
+        // implement PoW
+    }
+
     function validateTx(KaistBitcoinTx memory _tx) public pure returns (bool) {
         bytes32 txHash = _txHash(_tx);
         bytes32 hashToSign = ECDSA.toEthSignedMessageHash(txHash);
         address signer = ECDSA.recover(hashToSign, _tx.signature);
         require(_tx.from == signer, "Invalid signature");
         return true;
+    }
+
+    function _blockHash(
+        KaistBitcoinBlockHeader memory _block
+    ) internal pure returns (bytes32) {
+        // return block hash
     }
 
     function _txHash(
